b4718fc302cbdafad97e6d97553017f45658e3e8
Alina <maliniaka.web@gmail.com>
2018-02-26 19:16:39 +0100

    Added e-mail format and example e-mail, password validation

--- build/js/form-validation.js
+++ build/js/form-validation.js
@@ -27,27 +27,26 @@ $(document).ready(function() {
 				e.preventDefault();				
 				formValidation._clearErrors;
 
-				var valid = true;				
+				var valid = false;
+				var valid_email_format = false;				
 				var email_value = $('#form-fields input[name=email]').val().trim();
 				var password_value = $('#form-fields input[name=password]').val().trim();
-				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
 				
+				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;				
 				if (pattern.test(email_value)){
-					email = true;
-				} else {
-					email = false;
-					password = false;
+					valid_email_format = true;
 				}
 
+				var check_email_password = formValidation._checkEmailPassword(email_value,password_value);
+
 				if ( !email_value) {
-					valid = false;
 					$(empty_email).slideDown('500');
 				} else if ( !password_value ) {
-					valid = false;
 					$(empty_password).slideDown('500');
-				} else if ( !email && !password ) {
-					valid = false;
-					$(error_email_password).slideDown('500');
+				} else if ( !valid_email_format ) {
+					$(empty_email).slideDown('500').text('Введите email в формате example@mail.com');
+				} else if ( !check_email_password ) {
+					$(error_email_password).slideDown('500').append('<div class="added_test">Тестовый набор: test@mail.com пароль: 123qwe</div>');
 				} else {
 					valid = true;
 				}
@@ -62,6 +61,22 @@ $(document).ready(function() {
 
 			_clearErrors: function(){
 				errors_block.children('div').slideUp('500');
+				$('.added_test').remove();
+			},
+
+			_checkEmailPassword: function(email, password){
+				var valid = false;
+				var test_email = 'test@mail.com';
+				var test_password = '123qwe';
+				var form_action = $('#form-validate').attr( 'action' );
+
+				if ( email == test_email && password == test_password ) {
+					valid = true;
+				}else if (form_action == '/sign-up.html' && email != test_email ) {
+					valid = true;
+				}
+
+				return valid;
 			},
 
 			_submitForm: function(e){
